210de84338e8b653eb64c5c4f0d098ea37fea3a8
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-05-06 17:58:18 -0500

    alllows hosts

--- registro_medico/settings.py
+++ registro_medico/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-89v(!h()qw_(87)lxddqp5m@h-l@qb*qm$6mht2eirt531^7f1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
